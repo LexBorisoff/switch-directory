44a3e8fd63051d46ab50cc47ee046f19c72d7f8e
Lex Borisoff <hello@LexBorisoff.com>
2023-10-14 19:55:16 -0400

    small edit in the zip.sh script

--- zip.sh
+++ zip.sh
@@ -11,19 +11,19 @@ if [ $# -le 1 ]; then
 
 	# Linux x64
 	if [ $# = 0 ] || [ $1 = "all" ] || [ $1 = "linux" ]; then
-		rm dist/linux-x64.zip
+		rm -f dist/linux-x64.zip
 		zip -j dist/linux-x64.zip dist/bin/linux-x64/*
 	fi
 
 	# MacOS x64
 	if [ $# = 0 ] || [ $1 = "all" ] || [ $1 = "macos-x64" ]; then
-		rm dist/macos-x64.zip
+		rm -f dist/macos-x64.zip
 		zip -j dist/macos-x64.zip dist/bin/macos-x64/*
 	fi
 
 	# MacOS ARM
 	if [ $# = 0 ] || [ $1 = "all" ] || [ $1 = "macos-arm" ]; then
-		rm dist/macos-arm.zip
+		rm -f dist/macos-arm.zip
 		zip -j dist/macos-arm.zip dist/bin/macos-arm/*
 
 	fi
