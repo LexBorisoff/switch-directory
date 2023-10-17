f86a0e44bdd5c7dd13d39ba1a0df296cc263d55c
Lex Borisoff <hello@LexBorisoff.com>
2023-10-16 20:49:07 -0400

    updated zip.sh script

--- zip.sh
+++ zip.sh
@@ -5,26 +5,26 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ $# -le 1 ]; then
 	# Windows x64
 	if [ $# = 0 ] || [ $1 = "all" ] || [ $1 = "windows" ]; then
-		rm -f dist/windows-x64.zip
-		zip -j dist/windows-x64.zip dist/bin/windows-x64/*
+		rm -f dist/sd-windows-x64.zip
+		zip -j dist/sd-windows-x64.zip dist/bin/windows-x64/*
 	fi
 
 	# Linux x64
 	if [ $# = 0 ] || [ $1 = "all" ] || [ $1 = "linux" ]; then
-		rm -f dist/linux-x64.zip
-		zip -j dist/linux-x64.zip dist/bin/linux-x64/*
+		rm -f dist/sd-linux-x64.zip
+		zip -j dist/sd-linux-x64.zip dist/bin/linux-x64/*
 	fi
 
 	# MacOS x64
 	if [ $# = 0 ] || [ $1 = "all" ] || [ $1 = "macos-x64" ]; then
-		rm -f dist/macos-x64.zip
-		zip -j dist/macos-x64.zip dist/bin/macos-x64/*
+		rm -f dist/sd-macos-x64.zip
+		zip -j dist/sd-macos-x64.zip dist/bin/macos-x64/*
 	fi
 
 	# MacOS ARM
 	if [ $# = 0 ] || [ $1 = "all" ] || [ $1 = "macos-arm" ]; then
-		rm -f dist/macos-arm.zip
-		zip -j dist/macos-arm.zip dist/bin/macos-arm/*
+		rm -f dist/sd-macos-arm.zip
+		zip -j dist/sd-macos-arm.zip dist/bin/macos-arm/*
 
 	fi
 fi
